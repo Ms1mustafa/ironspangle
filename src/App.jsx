bd509491334936b5118c65cb0abea6fff11d280e
Mustafa <mustafaalasadi58@gmail.com>
2024-07-17 17:27:01 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SideBar from "./pages/SideBar.jsx";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const logo = `${import.meta.env.VITE_REACT_APP_PUBLIC_URL}/logo.png`;
+  const logo = import.meta.env.VITE_REACT_APP_PUBLIC_URL;
   return (
     <>
       <div className="bg-white h-full grid grid-cols-[auto,1fr]">
