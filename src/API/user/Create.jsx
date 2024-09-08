325543bb74ed6b14b2dacc6445389c1017a8fc5f
Mustafa <mustafaalasadi58@gmail.com>
2024-09-09 01:09:59 +0300

    fix env

---
+++ src/API/user/Create.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const Create = (data, setLoading, navigate) => {
+  setLoading(true);
+  axios
+    .post(`${import.meta.env.VITE_REACT_APP_API_URL}/user/create.php`, data)
+    .then((response) => {
+      setLoading(false);
+      if (response.status === 200) {
+        toast.success(response.data.message);
+        navigate(`/user`, {
+          replace: true,
+        });
+      } else {
+        toast.error(response.data.message);
+      }
+    })
+    .catch((error) => {
+      toast.error(error.response.data.message); // Displaying specific error message
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+};
+
+export default Create;
